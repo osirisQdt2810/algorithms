87244d79c437e09e3358c6c49432756f608ad2bb
phucnp <phucnguyenphi123@gmail.com>
2026-05-31 12:47:39 +0700

    [update] a brilliant approach about how to bucket an array based on value, not quantity (lc daily challenge 31-05-2026, prob 2126)

--- src/leetcode/daily/2026_05_31/2126. Destroying Asteroids.cpp
+++ src/leetcode/daily/2026_05_31/2126. Destroying Asteroids.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 class Solution {
 public:
+    // O(nlogn) time, O(n) space
     bool asteroidsDestroyedPQ(int mass, vector<int>& asteroids) {
         priority_queue<int, vector<int>, greater<int>> pq;
         for (auto& a: asteroids) pq.push(a);
@@ -22,7 +23,8 @@ public:
         return true;
     }
 
-    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+    // O(nlogn) time, O(1) space
+    bool asteroidsDestroyedSort(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(), asteroids.end());
 
         long long ll_mass = mass;
@@ -31,6 +33,41 @@ public:
             ll_mass += a;
         }
         return true;
+    }
+
+    // O(n) time, O(n) space
+    bool asteroidsDestroyedBucket(int mass, vector<int>& asteroids) {
+        // 1 ý tưởng rất hay => bucket-based approach => bucket như nào nếu ta không biết được max của asteroids?
+        // => ta bucket theo range của bit, chứ không bucket theo khoảng có giá trị bằng nhau
+        // => bucket[k] chứa phần tử thuộc [2^k, 2^(k+1)), gọi min[k] là phần tử min của bucket[k]
+        // => ta sẽ duyệt bucket từ nhỏ đến lớn, nếu tại k:
+        //        - min[k] > mass => return false
+        //        - min[k] <= mass => mass + min >= 2^k + 2^k > max(bucket[k]) => ta có thể ăn hết bucket[k] => mass += sum(bucket[k])
+        // => ta sẽ có O(n) time, O(n) space
+
+        using ll = long long;
+        constexpr int RANGE = 32;
+        vector<ll> sum_bucket(RANGE, 0);
+        vector<int> min_bucket(RANGE, INT_MAX);
+
+        for (auto& a : asteroids){
+            // int k = floor(log2(a));
+            // similar approach:
+            int k = RANGE - 1 - __builtin_clz(a); // clz: Count Leading Zeros => số lượng 0 liên tiếp nằm ở vị trí đầu tiên
+            sum_bucket[k] += a;
+            min_bucket[k] = min(min_bucket[k], a);
+        }
+        long long ll_mass = mass;
+        for (int k = 0; k < RANGE; ++k){
+            if (min_bucket[k] == INT_MAX) continue;
+            if (ll_mass < min_bucket[k]) return false;
+            ll_mass += sum_bucket[k];
+        }
+        return true;
+    }
+
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        return asteroidsDestroyedBucket(mass, asteroids);
     }
 };
 
